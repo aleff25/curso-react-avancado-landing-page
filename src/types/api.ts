bc4407e6d64748f4c740df92be3b18c99c21fc3f
Aleff <aleff2594@gmail.com>
2020-08-29 16:25:15 -0300

    Feat: adding graphql requests

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,83 @@
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type Modules = {
+  title: string
+  subtitle: string
+  description: string
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  media: {
+    alternativeText: string
+    url: string
+  }
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: Array<{
+    title: string
+  }>
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Modules[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
 }
